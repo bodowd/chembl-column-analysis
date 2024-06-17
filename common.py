42b05790afa44b6e069a3a5f3af35c7ec8446b1b
bodowd <odowd.bing@gmail.com>
2024-06-17 13:23:04 +0200

    refactor ndv

---
+++ common.py
@@ -0,0 +1,65 @@
+from psycopg import sql
+import psycopg
+import csv
+
+INTS = ["bigint", "smallint", "integer"]
+FLOATS = ["numeric"]
+TS = ["timestamp without time zone"]
+STRINGS = ["text", "character varying"]
+
+
+class Connection:
+    def __init__(self):
+        self.conn = psycopg.connect(
+            "postgresql://postgres:example@localhost:5435/chembl_33"
+        )
+
+    def __exit__(self):
+        self.conn.close()
+
+
+class MetaData:
+    def __init__(self):
+        self.data_types = []
+        self.tables = {}
+        self.get_data_types()
+        self.get_column_metadata()
+
+    def get_data_types(self):
+        with open("data_types.csv", "r") as f:
+            reader = csv.reader(f)
+            # skip header
+            next(reader)
+            for (data_type,) in reader:
+                self.data_types.append(data_type)
+
+    def get_column_metadata(self):
+        with open("column_metadata.csv", "r") as f:
+            reader = csv.reader(f)
+            # skip header
+            next(reader)
+
+            for table_name, column_name, data_type in reader:
+                if table_name not in self.tables:
+                    self.tables[table_name] = {}
+                    self.tables[table_name]["int"] = []
+                    self.tables[table_name]["float"] = []
+                    self.tables[table_name]["string"] = []
+                    self.tables[table_name]["timestamp"] = []
+
+                if data_type in INTS:
+                    self.tables[table_name]["int"].append(column_name)
+                elif data_type in FLOATS:
+                    self.tables[table_name]["float"].append(column_name)
+                elif data_type in TS:
+                    self.tables[table_name]["timestamp"].append(column_name)
+                elif data_type in STRINGS:
+                    self.tables[table_name]["string"].append(column_name)
+
+            # quick check
+            assert (
+                len(self.tables["assays"]["int"]) == 9
+            ), "should be 9 columns with integers in 'assays' table"
+            assert (
+                len(self.tables["assays"]["string"]) == 15
+            ), "should be 15 columns with character varying in 'assays' table"
--- ndv_ratio.py
+++ ndv_ratio.py
@@ -1,73 +1,41 @@
 from psycopg import sql
-import psycopg
 import csv
+from common import MetaData, Connection
 
-ints = ["bigint", "smallint", "integer"]
-floats = ["numeric"]
-ts = ["timestamp without time zone"]
-strings = ["text", "character varying"]
 
-data_types = []
-with open("data_types.csv", "r") as f:
-    reader = csv.reader(f)
-    # skip header
-    next(reader)
-    for (data_type,) in reader:
-        data_types.append(data_type)
+metadata = MetaData()
+tables = metadata.tables
+connection = Connection()
+conn = connection.conn
 
-with open("column_metadata.csv", "r") as f:
-    reader = csv.reader(f)
-    # skip header
-    next(reader)
 
-    tables = {}
-    for table_name, column_name, data_type in reader:
-        if table_name not in tables:
-            tables[table_name] = {}
-            tables[table_name]["int"] = []
-            tables[table_name]["float"] = []
-            tables[table_name]["string"] = []
-            tables[table_name]["timestamp"] = []
+def compute_ndv(cur, column_name, table_name):
+    cur.execute(
+        sql.SQL(
+            """
+        SELECT 
+            COUNT(DISTINCT({col})) / NULLIF(cast(COUNT({col}) as decimal),0), 
+            COUNT(DISTINCT({col})), 
+            COUNT({col})
+        FROM {tbl}
+        """
+        ).format(
+            col=sql.Identifier(column_name),
+            tbl=sql.Identifier(table_name),
+        )
+    )
 
-        if data_type in ints:
-            tables[table_name]["int"].append(column_name)
-        elif data_type in floats:
-            tables[table_name]["float"].append(column_name)
-        elif data_type in ts:
-            tables[table_name]["timestamp"].append(column_name)
-        elif data_type in strings:
-            tables[table_name]["string"].append(column_name)
 
-    # quick check
-    assert (
-        len(tables["assays"]["int"]) == 9
-    ), "should be 9 columns with integers in 'assays' table"
-    assert (
-        len(tables["assays"]["string"]) == 15
-    ), "should be 15 columns with character varying in 'assays' table"
-
-with psycopg.connect("postgresql://postgres:example@localhost:5435/chembl_33") as conn:
+with open("ndv.csv", "w") as f:
+    writer = csv.writer(f)
+    writer.writerow(
+        ["table_name", "data_type", "column_name", "ndv", "count_distinct", "count"]
+    )
     with conn.cursor() as cur:
         for tbl in tables.keys():
             for _type in tables[tbl].keys():
                 for c in tables[tbl][_type]:
-                    cur.execute(
-                        sql.SQL(
-                            """
-                        SELECT 
-                            COUNT(DISTINCT({})) / NULLIF(cast(COUNT({}) as decimal),0), 
-                            COUNT(DISTINCT({})), 
-                            COUNT({})
-                        FROM {}
-                        """
-                        ).format(
-                            sql.Identifier(c),
-                            sql.Identifier(c),
-                            sql.Identifier(c),
-                            sql.Identifier(c),
-                            sql.Identifier(tbl),
-                        )
-                    )
-
+                    compute_ndv(cur, c, tbl)
                     for ndv, count_distinct, count in cur:
+                        writer.writerow([tbl, _type, c, ndv, count_distinct, count])
                         print(tbl, _type, c, ndv, count_distinct, count)
